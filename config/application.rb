e979c343024e23df01c5bf2c76294e36c232cf9d
botiuk <svetabotiuk@gmail.com>
2024-07-22 12:47:04 +0300

    add gem raills-i18n

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,11 @@ module Tfa
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Supported locales
+    config.i18n.available_locales = [:uk, :en]
+
+    # Default locale
+    config.i18n.default_locale = :uk
   end
 end
