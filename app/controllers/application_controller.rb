2cbb1184d6b70bc19642e7ef967e991955c009e7
botiuk <svetabotiuk@gmail.com>
2024-07-24 13:30:33 +0300

    add rescue from cancan access denied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     include Pagy::Backend
+
+    rescue_from CanCan::AccessDenied do |exception|
+        redirect_to root_url, alert: t('alert.access_denied')
+    end
 end
