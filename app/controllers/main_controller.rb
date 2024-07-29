8e3e361790801f4248b09d84f10056dc5eb168bb
botiuk <svetabotiuk@gmail.com>
2024-07-29 13:45:38 +0300

    changed slideshow on home page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,5 +2,6 @@ class MainController < ApplicationController
     authorize_resource :class => false
     
     def index
+        @news_stories = NewsStory.where.not(published_at: nil).where.not('published_at > ?', DateTime.now).order(published_at: :desc).limit(5)
     end
 end
